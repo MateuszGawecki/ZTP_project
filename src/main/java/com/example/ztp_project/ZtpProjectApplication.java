1f2eb43b67e0bd69618e5bcbb25ccb8e1922c273
MateuszGawecki <72608321+MateuszGawecki@users.noreply.github.com>
2023-06-02 14:51:37 +0200

    init

---
+++ src/main/java/com/example/ztp_project/ZtpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ztp_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZtpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZtpProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ztp_project/ZtpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ztp_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZtpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
